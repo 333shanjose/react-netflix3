4d920e70b9133e423ff977f1ba233814fdf6ed6e
shan jose <shanjose32@gmail.com>
2022-08-22 13:11:32 +0530

    url file

---
+++ src/url.js
@@ -0,0 +1,3 @@
+import {API_KEY} from "./constants/constants"
+ export const action=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=28`
+export const adventure=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&with_genres=12`
