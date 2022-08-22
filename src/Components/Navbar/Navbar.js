d005b22cc150adb8d88028cc129400f9c04c0fea
shan jose <shanjose32@gmail.com>
2022-08-22 12:55:42 +0530

    navbar.js file

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css';
+function Navbar() {
+  return (
+    <div className="navbar">
+       <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/800px-Netflix_2015_logo.svg.png?20190206123158" alt="netflix"/>
+       <img className="avatar" alt="avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"/>
+    </div>
+  )
+}
+
+export default Navbar
